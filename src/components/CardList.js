4b4c969a8bf781841e578eaee104bf14033e43ab
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-08 10:47:52 -0500

    fixed key error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -6,7 +6,7 @@ const CardList = ({ robots }) => {
 			{robots.map((user, i) => {
 				return (
 					<Card
-						key={robots.id}
+						key={i}
 						id={robots[i].id}
 						name={robots[i].name}
 						email={robots[i].email}
