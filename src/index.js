e1676b07d08338840e48b863661c316c0b9edefc
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-08 10:35:15 -0500

    final version with state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import App from "./containers/App";
+
 import "./index.css";
-import App from "./App";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
